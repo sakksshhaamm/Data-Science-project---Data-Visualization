97aab4e42b68f062b7acaa57b31d6b094eb8197f
Abhyansh Agrahari <50028089+jam-abhyansh06@users.noreply.github.com>
2020-04-10 01:45:19 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,10 +1,10 @@
 import subprocess
 
 #adds googletrans module
-subprocess.call('(pip3 install googletrans && pip3 install google-speech)', shell = True)
+subprocess.call('(pip3 install googletrans)', shell = True)
 
-#adds google-speecch module
-subprocess.call('()', shell = True)
+#adds google-speech module
+subprocess.call('(pip3 install google-speech)', shell = True)
 
 #installs sox library for google-speech
 subprocess.call('(sudo apt-get install sox libsox-fmt-mp3)', shell = True)
